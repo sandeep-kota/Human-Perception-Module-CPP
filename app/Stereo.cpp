87e24d0a8b00be7d3ebfe8ca1734f7e57cb6a2a2
Satyarth Praveen <satyarth934@gmail.com>
2019-10-21 15:45:45 -0400

    #[2.4 : STC] Stereo Module implementation.

--- app/Stereo.cpp
+++ app/Stereo.cpp
@@ -1,28 +1,81 @@
 /**
- * @copyright Copyright (c) Sandeep Kota Sai Pavan
- * @copyright Copyright (c) Satyarth Praveen
- * @copyright 3-Clause BSD License
+ * @copyright BSD 3-Clause License 
+ * Copyright (c) 2019, @author Sandeep Kota Sai Pavan
+ * Copyright (c) 2019, @author Satyarth Praveen
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met: 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer. 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  * @file Stereo.cpp
- * @brief Stereo class implementation.It contains the function 
- * definitions to compute disparity for distance estimation.
+ * @brief Stereo class implementation.
  *
- * @author Sandeep Kota Sai Pavan
- * @author Satyarth Praveen
- * @date 10/14/2019
+ * @date 10/20/2019
  */
 
-#include <iostream>
-#include <opencv2/opencv.hpp>
 #include <Stereo.hpp>
 
 Stereo::Stereo() {
-    cv::Ptr<cv::StereoBM> bm;
-    double camera_focal_length = 0;  //  Random stub values.
-    double camera_baseline = 0;  //  Random stub values.
+    int num_disparities = 64;
+    int block_size = 15;
+	bm = cv::StereoBM::create(num_disparities, block_size);
+    // bm->setROI1(roi1);
+    // bm->setROI2(roi2);
+    bm->setPreFilterCap(31);
+    bm->setPreFilterSize(9);
+    // blockSize: 15
+    // bm->setBlockSize(15);
+    bm->setMinDisparity(0);
+    bm->setNumDisparities(128);
+    bm->setTextureThreshold(0);
+    bm->setUniquenessRatio(1);
+    bm->setSpeckleWindowSize(250);
+    bm->setSpeckleRange(10);
+    bm->setDisp12MaxDiff(1);
+/*=======
+	bm = cv::StereoBM::create(16,5);
+    bm -> setPreFilterCap(31);
+    bm -> setBlockSize(5);
+    bm -> setMinDisparity(0);
+    bm -> setTextureThreshold(10);
+    bm -> setUniquenessRatio(15);
+    bm -> setSpeckleWindowSize(100);
+    bm -> setSpeckleRange(32);
+    bm -> setDisp12MaxDiff(1);
+*/
 }
 
-cv::Mat Stereo::getDisparity(cv::Mat imgL, cv::Mat imgR) {
-    cv::Mat disparity = cv::Mat::zeros(100, 100, 1);  //  Random stub values.
+Stereo::~Stereo() {}
+
+cv::Mat Stereo::getDisparity(cv::Mat img_l, cv::Mat img_r) {
+    cv::Mat img_l_gray, img_r_gray;
+	cv::cvtColor(img_l, img_l_gray, cv::COLOR_BGR2GRAY);
+	img_l_gray.convertTo(img_l_gray, CV_8U);
+	cv::cvtColor(img_r, img_r_gray, cv::COLOR_BGR2GRAY);
+	img_r_gray.convertTo(img_r_gray, CV_8U);
+
+    cv::Mat disparity;
+    bm->compute(img_l_gray, img_r_gray, disparity);
+
+    disparity.convertTo(disparity, CV_32F, 1.0/16.0);
+
+    // cv::Mat disparity = cv::Mat::zeros(100, 100, 1);  //  Random stub values.
     return disparity;
 }
--- include/Stereo.hpp
+++ include/Stereo.hpp
@@ -1,14 +1,33 @@
 /**
- * @copyright Copyright (c) Sandeep Kota Sai Pavan
- * @copyright Copyright (c) Satyarth Praveen
- * @copyright 3-Clause BSD License
+ * @copyright BSD 3-Clause License 
+ * Copyright (c) 2019, @author Sandeep Kota Sai Pavan
+ * Copyright (c) 2019, @author Satyarth Praveen
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met: 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer. 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  * @file Stereo.hpp
  * @brief Stereo class definition.
  *
- * @author Sandeep Kota Sai Pavan
- * @author Satyarth Praveen
- * @date 10/14/2019
+ * @date 10/20/2019
  */
 
 #ifndef INCLUDE_STEREO_HPP_
@@ -23,9 +42,8 @@
 class Stereo {
  private:
     cv::Ptr<cv::StereoBM> bm;
-    double camera_focal_length;
-    double camera_baseline;
-
+    cv::Mat disparity_image;
+    
  public:
     /**
      * @brief A constructor for Stereo class.
@@ -48,7 +66,7 @@ class Stereo {
      * @param cv::Mat Right image
      * @return cv::Mat Disparity map
      */
-    cv::Mat getDisparity(cv::Mat imgL, cv::Mat imgR);
+    cv::Mat getDisparity(cv::Mat img_l, cv::Mat img_r);
 };
 
 #endif  // INCLUDE_STEREO_HPP_
